05d17843dae08ffb5c96bcf7521532c34bbe0be3
Николай Шведов <shved.nikolay.18@mail.ru>
2022-01-10 17:10:02 +0300

    update last commit
    update last commit

--- WindowsFormsApp1/VideoSystem/BitBltForm.Designer.cs
+++ WindowsFormsApp1/VideoSystem/BitBltForm.Designer.cs
@@ -28,13 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запустить работу функции";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // BitBltForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(680, 339);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(348, 212);
+            this.Controls.Add(this.button1);
             this.Name = "BitBltForm";
             this.Text = "BitBltForm";
             this.ResumeLayout(false);
@@ -42,5 +55,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
--- WindowsFormsApp1/VideoSystem/BitBltForm.cs
+++ WindowsFormsApp1/VideoSystem/BitBltForm.cs
@@ -1,20 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1.VideoSystem
 {
     public partial class BitBltForm : Form
     {
+
+        public static Int32 SRCINVERT = 0x00660046;
+
+        [DllImportAttribute("gdi32.dll")]
+        private static extern int BitBlt(
+            IntPtr hdcDest,     // handle to destination DC (device context)
+            int nXDest,         // x-coord of destination upper-left corner
+            int nYDest,         // y-coord of destination upper-left corner
+            int nWidth,         // width of destination rectangle
+            int nHeight,        // height of destination rectangle
+            IntPtr hdcSrc,      // handle to source DC
+            int nXSrc,          // x-coordinate of source upper-left corner
+            int nYSrc,          // y-coordinate of source upper-left corner
+            Int32 dwRop  // raster operation code
+      );
+
+        [DllImport("gdi32.dll", EntryPoint = "SelectObject")]
+        public static extern IntPtr SelectObject(IntPtr hdc, IntPtr h);
+
+        public void TestXOR()
+        {
+            Bitmap bmp = new Bitmap(100, 100);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawLine(Pens.White, new Point(0, 0), new Point(100, 100));
+            bmp.Save(@"D:\testBefore.bmp", ImageFormat.Bmp);
+            IntPtr testHDC = g.GetHdc();
+            SelectObject(testHDC, bmp.GetHbitmap());
+            BitBlt(testHDC, 0, 0, 100, 100, testHDC, 0, 0, SRCINVERT);
+            g.ReleaseHdc(testHDC);
+            bmp.Save(@"D:\testAfter.bmp", ImageFormat.Bmp);
+            g.Dispose();
+
+            MessageBox.Show("Функция успешно отработала!", "Выполнение функции BitBlt!");
+        }
+
         public BitBltForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BitBltForm tst = new BitBltForm();
+            tst.TestXOR();
+            tst.Dispose();
         }
     }
 }
